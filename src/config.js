10c33f126172bc066430425121e894ccf356acbd
Sincerely George <SincerelyGeorge@gmail.com>
2020-05-27 00:08:21 -0500

    Setting up React App & router basics

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "ia-notes-app-uploads"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://69jwdfu8dl.execute-api.us-east-2.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_cNPizIoi3",
+      APP_CLIENT_ID: "20jjnt26iocad5c53l4gm2vdff",
+      IDENTITY_POOL_ID: "us-east-2:2dcda5a3-3bbd-40bd-a2e5-5d1e4ce783db"
+    }
+};
