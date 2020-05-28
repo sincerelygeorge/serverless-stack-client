bb97d1d51536b53e103aa613fdf63c18eb7e3520
Sincerely George <SincerelyGeorge@gmail.com>
2020-05-27 19:59:51 -0500

    My first React Functional App

--- src/config.js
+++ src/config.js
@@ -1,16 +1,18 @@
 export default {
-    s3: {
-      REGION: "us-east-2",
-      BUCKET: "ia-notes-app-uploads"
-    },
-    apiGateway: {
-      REGION: "us-east-2",
-      URL: "https://69jwdfu8dl.execute-api.us-east-2.amazonaws.com/prod"
-    },
-    cognito: {
-      REGION: "us-east-2",
-      USER_POOL_ID: "us-east-2_cNPizIoi3",
-      APP_CLIENT_ID: "20jjnt26iocad5c53l4gm2vdff",
-      IDENTITY_POOL_ID: "us-east-2:2dcda5a3-3bbd-40bd-a2e5-5d1e4ce783db"
-    }
+  COPYMAX_ATTACHMENT_SIZE: 5000000,
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "ia-notes-app-uploads",
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://69jwdfu8dl.execute-api.us-east-2.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_cNPizIoi3",
+    APP_CLIENT_ID: "20jjnt26iocad5c53l4gm2vdff",
+    IDENTITY_POOL_ID: "us-east-2:2dcda5a3-3bbd-40bd-a2e5-5d1e4ce783db",
+  },
+  STRIPE_KEY: "pk_test_f3Go2gYRxhq5YsKoXyMXtP9d00BAm9k3Fo",
 };
